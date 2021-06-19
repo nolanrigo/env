e1ed6dfad33c0e1381f3a61826fbc1386ed4cf2c
Nolan Rigo <nolan@rigo.dev>
2021-06-19 15:33:56 +0200

    feat(env,envAsNumber): defaultValue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export function env(key: string): string {
-  const value = process.env[key];
+export function env(key: string, defaultValue?: string): string {
+  const value = process.env[key] ?? defaultValue;
 
   if (!value) {
     throw new Error(`Fail to retreive, process.env.${key} is not defined`);
@@ -8,8 +8,8 @@ export function env(key: string): string {
   return value;
 }
 
-export function envAsNumber(key: string): number {
-  const value = Number(env(key));
+export function envAsNumber(key: string, defaultValue?: number): number {
+  const value = Number(env(key, defaultValue?.toString()));
 
   if (Number.isNaN(value)) {
     throw new Error(`process.env.${key} is not a number`);
